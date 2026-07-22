8ef60e9eb02ed1818b39542ec6ba54a92f4b3d07
risvana423 <risvanarisvana131@gmail.com>
2026-07-22 09:13:29 +0530

    Time: 0 ms (100%), Space: 19.5 MB (9.53%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,39 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head or not head.next or k == 0:
+            return head
+        
+        # Find length and tail
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+        
+        # Modulo reduction
+        k %= length
+        if k == 0:
+            return head
+        
+        # Form cycle
+        tail.next = head
+        
+        # Find new tail
+        steps_to_new_tail = length - k - 1
+        new_tail = head
+        for _ in range(steps_to_new_tail):
+            new_tail = new_tail.next
+        
+        # Break cycle
+        new_head = new_tail.next
+        new_tail.next = None
+        
+        return new_head
+
+
+        
